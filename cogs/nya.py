daca025f920d1ba92522b2d5b8cd92d9e57cb6e5
Xomashok <79464833+Xomashok@users.noreply.github.com>
2024-03-12 14:53:28 +0800

    nya add

--- cogs/nya.py
+++ cogs/nya.py
@@ -1,26 +1,36 @@
 import disnake 
 from disnake.ext import tasks, commands
 import random
+import re
 
 class Nya(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
         self.nya.start()
 
     @commands.command()
     async def Nya(self, inter, chanel : str):
-        data = open('data/nya.txt', 'w')
-        data.write(chanel)
-        data.close
-        await inter.response.send_message(f'Канал записан')
+        guild_id = inter.guild.id
+        wr = re.sub(r'\D', '', chanel)
+        data = open('data\\nya.txt', 'w')
+        data.write(f'{guild_id}\n{wr}')
+        data.close()
+        await inter.send(f'Канал записан')
 
     @tasks.loop(hours=1)
-    async def nya():
+    async def nya(self):
         nya = ('Мя', 'Мяу', 'Ня')
-        ran = random.randint(0, 2)
-        data = open('data/nya.txt')
-        channel = bot.get_channel(data.read())
-        await channel.send(nya[ran])
+        with open('data\\nya.txt') as data:
+            lines = data.readlines()
+            guild_id = int(lines[0])
+            channel_id = int(lines[1])
+            guild = self.bot.get_guild(guild_id)
+            if guild:
+                channel = guild.get_channel(channel_id)
+                if channel:
+                    await channel.send(random.choice(nya))
+
 
 def setup(bot):
     bot.add_cog(Nya(bot))
